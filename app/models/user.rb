496319f56554527d11e779f701a6a3fb126907df
nakakoki <71761751+nakakoki@users.noreply.github.com>
2020-10-20 15:19:47 +0900

    修正しました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,12 @@ class User < ApplicationRecord
   with_options presence: true, format: { with: /\A[ぁ-んァ-ンー-龥]+\z/ } do
     validates :last_name
     validates :first_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } do
     validates :last_name_kana
     validates :first_name_kana
   end
+
   validates :birth_day, presence: true
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
