7cef4d3777ed5fd39e3ff7510fca954f21981e59
nakakoki <71761751+nakakoki@users.noreply.github.com>
2020-10-24 20:15:02 +0900

    修正しました。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,7 +35,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if @item.user.id
+    if current_user.id == @item.user.id
       @item.destroy
       redirect_to root_path
     end
